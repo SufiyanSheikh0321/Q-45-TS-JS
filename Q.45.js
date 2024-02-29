e00b400b42f647edda4fe963567fd90a2c0062a0
Muhammed Sufiyan <148213204+SufiyanSheikh0321@users.noreply.github.com>
2024-02-29 21:34:42 +0500

    Add files via upload

---
+++ Q.45.js
@@ -0,0 +1,12 @@
+"use strict";
+// 45. Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+Object.defineProperty(exports, "__esModule", { value: true });
+function createCar(manufacturer, model, ...options) {
+    let car = { manufacturer, model };
+    options.forEach(([key, value]) => {
+        car[key] = value;
+    });
+    return car;
+}
+let myCar = createCar('Audi', 'A6', ['color', 'black'], ['year', '2024']);
+console.log(myCar);
